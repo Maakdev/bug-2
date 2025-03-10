3b06d3b05f351161ae957018640020ce15db7a58
Maakdev <mikhael.dev@proton.me>
2025-03-10 08:30:29 +0200

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>{{ count }}</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component'
+
+import { Emit } from 'vue-property-decorator'
+
+// Component definition
+@Options({
+  // Define component options
+  watch: {
+    count: value => {
+      console.log(value)
+    }
+  }
+})
+export default class Counter extends Vue {
+  @Emit('test')
+  test {
+    return 'TEST'
+  }
+
+  // The behavior in class is the same as the current
+  count = 0
+
+  increment() {
+    this.count++
+  }
+}
+</script>
